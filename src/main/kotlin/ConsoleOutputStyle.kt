96c118b4cb8bc13515906ac19dfc6f0c16bc9981
Ben Green <s.edwin.green@gmail.com>
2025-01-09 21:34:54 -0500

    Abstract stylized string creation

--- src/main/kotlin/ConsoleOutputStyle.kt
+++ src/main/kotlin/ConsoleOutputStyle.kt
@@ -7,41 +7,35 @@ enum class ConsoleOutputStyle {
         override fun encode(character: String, letterStatus: LetterStatus): String =
             when (letterStatus) {
                 LetterStatus.Correct ->
-                    StylizedStringBuilder(character)
-                        .style(Style.Bold)
-                        .text(TextColor.Black)
-                        .background(BackgroundColor.Green)
-                        .create()
+                    createStylizedString(character, Style.Bold, TextColor.Black, BackgroundColor.Green)
                 LetterStatus.Almost ->
-                    StylizedStringBuilder(character)
-                        .style(Style.Italic)
-                        .text(TextColor.Black)
-                        .background(BackgroundColor.Yellow)
-                        .create()
+                    createStylizedString(character, Style.Italic, TextColor.Black, BackgroundColor.Yellow)
                 LetterStatus.Unused ->
-                    StylizedStringBuilder(character)
-                        .style(Style.Dim)
-                        .text(TextColor.White)
-                        .background(BackgroundColor.Black)
-                        .create()
+                    createStylizedString(character, Style.Dim, TextColor.White, BackgroundColor.Black)
             }
+
+        private fun createStylizedString(character: String, style: Style, textColor: TextColor, backgroundColor: BackgroundColor): String =
+            StylizedStringBuilder(character)
+                .style(style)
+                .text(textColor)
+                .background(backgroundColor)
+                .create()
     },
     BlackAndWhite {
         override fun encode(character: String, letterStatus: LetterStatus): String =
             when (letterStatus) {
                 LetterStatus.Correct ->
-                    StylizedStringBuilder(character)
-                        .style(Style.Bold)
-                        .create()
+                    createStylizedString(character, Style.Bold)
                 LetterStatus.Almost ->
-                    StylizedStringBuilder(character)
-                        .style(Style.Italic)
-                        .create()
+                    createStylizedString(character, Style.Italic)
                 LetterStatus.Unused ->
-                    StylizedStringBuilder(character)
-                        .style(Style.Dim)
-                        .create()
+                    createStylizedString(character, Style.Dim)
             }
+
+        private fun createStylizedString(character: String, style: Style): String =
+            StylizedStringBuilder(character)
+                .style(style)
+                .create()
     };
 
     abstract fun encode(character: String, letterStatus: LetterStatus): String
